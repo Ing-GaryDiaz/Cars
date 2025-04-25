664ca161c2e489a366165a8492fd16bf577783ed
Ing-GaryDiaz <ing.garydiaz@gmail.com>
2025-04-24 20:30:07 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -63,7 +63,8 @@ public class Main {
             System.out.println(hondaCopy);
         }
         catch (IOException | ClassNotFoundException e){
-            System.out.println(e);}
+            System.out.println(e);
+        }
     }
 
 }
